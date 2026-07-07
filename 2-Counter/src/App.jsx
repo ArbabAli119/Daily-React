7c5f35f359250875de5538f2fbe28a69a1dd6983
ArbabAli119 <arbabali361@gmail.com>
2026-07-08 02:12:21 +0530

    createCounterApp

--- 2-Counter/src/App.jsx
+++ 2-Counter/src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    console.log("clicked", counter);
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
 
   return (
     <>
-     <h1>Chai Aur Arbab</h1>
+      <h1>Chai Aur Arbab</h1>
+      <h2>Counter Value : {counter} </h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
