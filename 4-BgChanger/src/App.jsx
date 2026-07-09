5ff0356d3dbf4da5cb01a1303a45a0098bead536
ArbabAli119 <arbabali361@gmail.com>
2026-07-09 05:47:50 +0530

    increase px

--- 4-BgChanger/src/App.jsx
+++ 4-BgChanger/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         style={{ backgroundColor: color }}
       >
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-emerald-400 px-3 py-2 rounded-3xl">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-emerald-400 px-4 py-2 rounded-3xl">
             <button
               onClick={() => setColor("red")}
               className="outline-none px-7 py-3 rounded-full text-white shadow-2xl"
